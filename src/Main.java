edac94402404ca49b255bd3a5b180587c5bfbfa7
ulviabasli <162096919+ulviabasli@users.noreply.github.com>
2024-03-04 00:31:46 -0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world! dh hffhfhfhfg fd fd");
         System.out.println("Ssssssssssssssssssssss");
+        System.out.println("Ssssssssssssssssssssss");
 
     }
 }
